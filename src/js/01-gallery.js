f612be18380449c9fa578012981c93c0862750b4
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2022-12-14 20:46:57 +0200

    add SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+function makeGalleryMarkup(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href=${original}>
+        <img
+          class="gallery__image"
+          src=${preview}
+          data-source=${original}
+          alt=${description}
+        />
+      </a>
+    </div>`;
+    })
+    .join('');
+}
+
+refs.gallery.insertAdjacentHTML('afterbegin', makeGalleryMarkup(galleryItems));
+
+let gallery = new SimpleLightbox('.gallery a');
+
+gallery.on('show.simplelightbox', function () {
+  gallery.options.captionDelay = 250;
+  gallery.options.captionsData = 'alt';
+});
