a15d22dbb54de7570fa120944ff989f19662667c
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2022-12-15 13:37:17 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,14 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 const PLAYER_CURRENT_TIME_KEY = 'videoplayer-current-time';
 
-player.on('play', function () {
-  player.setCurrentTime(getCurrentTimeValue());
-});
+player.setCurrentTime(getCurrentTimeValue());
 
-player.on('timeupdate', saveCurrentTimeValue);
+player.on('timeupdate', throttle(saveCurrentTimeValue, 1000));
 
 function saveCurrentTimeValue(data) {
   localStorage.setItem(PLAYER_CURRENT_TIME_KEY, data.seconds);
