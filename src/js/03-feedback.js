2bbac46cc8f6da9622f497df5f5f3cc679475128
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2022-12-15 15:44:42 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+let formData = {};
+const FORM_DATA_KEY = 'feedback-form-state';
+
+if (localStorage.getItem(FORM_DATA_KEY)) {
+  const { email, message } = JSON.parse(localStorage.getItem(FORM_DATA_KEY));
+  refs.form.email.value = email || '';
+  refs.form.message.value = message || '';
+}
+
+refs.form.addEventListener('input', throttle(onFormInput, 1000));
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+
+  localStorage.setItem(FORM_DATA_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (!event.currentTarget.email.value) {
+    alert('Please enter your email!');
+    return;
+  }
+
+  console.log(formData);
+
+  formData = {};
+  event.currentTarget.reset();
+  localStorage.removeItem(FORM_DATA_KEY);
+}
